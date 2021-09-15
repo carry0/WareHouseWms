082ef33bf93430b8f759e857a31b061b1aea48f4
WigerCheng <wigercheng@gmail.com>
2021-09-15 14:36:40 +0800

    1、新增一个功能 2、添加内测泄露检测

---
+++ common/src/main/java/com/yf/common/base/OnItemClickListener.java
@@ -0,0 +1,17 @@
+package com.yf.common.base;
+
+import android.view.View;
+
+/**
+ * @Author
+ * @cerate 2021/9/6 16:43
+ **/
+public interface OnItemClickListener<T> {
+    /**
+     *
+     * @param view view
+     * @param data T
+     * @param position 下标
+     */
+    void onItemClick(View view,T data, int position);
+}
